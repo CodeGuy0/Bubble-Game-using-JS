03571e74d0889be47f1862ffedb8ab0862678980
Pranay Kumar Jha <125690497+CodeGuy0@users.noreply.github.com>
2024-11-18 23:43:31 +0530

    Updated files with latest changes

--- script.js
+++ script.js
@@ -1,7 +1,58 @@
-var clutter = "";
-
-for (var i=1; i<=102; i++){
-    clutter += `<div class="bubble">5</div>`;
+function makeBubble(){
+    var clutter = "";
+    for (var i=1; i<=102; i++){
+        var rn = Math.floor(Math.random()*10)
+        clutter += `<div class="bubble">${rn}</div>`;
+    }
+    document.querySelector("#pbtm").innerHTML = clutter;
 }
 
-document.querySelector("#pbtm").innerHTML = clutter;
+
+var timer = 60;
+function runTimer(){
+    var timerint = setInterval(function(){
+        if (timer > 0) {
+            timer--;
+            document.querySelector("#timerval").textContent = timer;
+        }
+        else {
+            clearInterval(timerint);
+            document.querySelector("#pbtm").innerHTML = `<h1>Game Over<h1>`;
+        }
+    }, 1000);
+}
+
+var hitrn = 0;
+function getNewHit(){
+     hitrn = Math.floor(Math.random()*10);
+     document.querySelector("#hitval").textContent = hitrn;
+}
+
+ var score = 0;
+
+ function increaseScore(){
+    score += 10;
+     document.querySelector("#scoreval").textContent = score;
+ }
+
+ document.querySelector("#pbtm")
+ .addEventListener("click",function (dets) {
+    var clickednum = Number(dets.target.textContent);
+    if(clickednum === hitrn){
+        increaseScore();
+        makeBubble();
+        getNewHit(); 
+    }
+ });
+
+
+
+
+
+
+
+
+runTimer();
+makeBubble();
+getNewHit();
+// increaseScore();
